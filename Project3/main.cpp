24a92f00dede65d578987278be85be469f790cc7
k2supra <andrijkorolevic@gmail.com>
2023-01-10 19:27:16 +0200

    DONE)

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main()
 {
-	cout << "Hello" << endl;
+	size_t y;  cin >> y;  cout << "In " << y << " year = " << 365 + (!(y % 4) ? !(y % 100) ? !(y % 400) ? true : false : true : false) << " days.";
 };
 
