09992dcceab226adb43a1312979a88e10dbef156
k2supra <andrijkorolevic@gmail.com>
2023-01-10 20:26:31 +0200

    not done

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 int main()
-{
-	cout << "Hello" << endl;
+{	
+	size_t grn; size_t kop; cout << "Enter grivna -> "; cin >> grn; cout << "Enter kopijky -> "; cin >> kop; cout << ((kop > 100 ? ++grn : true) ? grn : true) << "grn  " << (100 > kop ? kop : true) << "kop" << endl;
+	return 0;
 };
 
