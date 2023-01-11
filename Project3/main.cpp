c38495036fc100bcf5361048ddae9e60758fe06f
k2supra <andrijkorolevic@gmail.com>
2023-01-11 13:14:37 +0200

    done

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -2,12 +2,8 @@
 using namespace std;
 int main()
 {
-	size_t grn; size_t kop; cout << "Enter grivna -> "; cin >> grn; cout << "Enter kopijky -> "; cin >> kop; cout << ((kop > 100 ? ++grn : true) ? grn : true) << "grn  " << ((100 > kop) ? kop : kop - 100) << "kop" << endl;
-	//size_t x;
-	//size_t y;
-	//cin >> x;
-	//cin >> y;
-	//size_t k = x / y;
-	//cout << x / k <<endl;
+	size_t grn; size_t kop; cout << "Enter grivna -> "; cin >> grn; cout << "Enter kopijky -> "; cin >> kop; double k = kop % 100; double f = (kop - k) / 100; cout << grn + f << "grn  " << k << "kop" << endl;
+	//size_t grn; size_t kop; cout << "Enter grivna -> "; cin >> grn; cout << "Enter kopijky -> "; cin >> kop; cout << ((kop > 100 ? ++grn : true) ? grn : true) << "grn  " << (100 > kop ? kop : true) << "kop" << endl;
 	return 0;
 };
+
