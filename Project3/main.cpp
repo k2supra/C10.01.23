b13a825c21ebf4079989fa47bb27f96b201873a2
k2supra <andrijkorolevic@gmail.com>
2023-01-11 14:02:10 +0200

    done

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,7 +1,13 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main()
 {
-	cout << "Hello" << endl;
-};
+	double r;
+	cout << "Measuring a size of a ball. \nEnter a data (cm): \n   Radius: "; cin >> r;
+	double s;
+	s = (static_cast<double>(4) / 3) * M_PI * pow(r, 3);
+	cout << "Size (V) = " << s << "cube cm";
 
+};
