e4d65f107c70b0b039f9ea61b0b5a5b8ebc6fa36
k2supra <andrijkorolevic@gmail.com>
2023-01-11 13:31:51 +0200

    done

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 int main()
 {
-	cout << "Hello" << endl;
+	size_t l;
+	size_t w;
+	size_t h;
+	cout << "Measuring a size of a parallelepiped. \nEnter a data (cm): \n   Length: "; cin >> l; 
+	cout << "   Width: "; cin >> w;
+	cout << "   Height: "; cin >> h;
+	size_t s;
+	s = l * w * h;
+	cout << "Size (V) = " << s << "cube cm";
+
 };
 
